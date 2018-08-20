9e89b4a3acc54806647a66ec531dc0b9425e015e
AKitty88 <a.kitty02@gmail.com>
2018-08-20 18:41:53 +1000

    test errors fixed

--- word_ladder_test.py
+++ word_ladder_test.py
@@ -1,13 +1,14 @@
 import unittest
-from word_ladder import find
+from word_ladder import find_shortest
 from word_ladder import words
 
 
 class TestWordLadder(unittest.TestCase):
+
     def test_word_normal_start_end(self):
         # Test with normal start and target word
-        self.assertEqual(len(find("lead", "gold", words)), 4)
+        self.assertEqual(len(find_shortest("lead", "gold", words)), 4)
 
     def test_word_bad_start_normal_end(self):
         # Test with bad start and normal target word
-        self.assertEqual(len(find("lsss", "gold", words)), 0)
+        self.assertEqual(len(find_shortest("lsss", "gold", words)), 0)
