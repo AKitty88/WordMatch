218366256216d09752f9571da0ca55c101b041c4
AKitty88 <a.kitty02@gmail.com>
2018-08-22 19:13:32 +1000

    All tests finished

--- word_ladder_test.py
+++ word_ladder_test.py
@@ -33,13 +33,31 @@ class TestWordLadder(unittest.TestCase):
         self.assertEqual(len(main_for_test("lead", "gin", "tin", "y")), 0)
         self.assertEqual(len(main_for_test("lead", "gin", "lean", "y")), 0)
 
+    def test_short_numbers(self):
+        # Test the shortest path creator function with numbers
+        self.assertEqual(len(main_for_test("45", "55", "", "Y")), 0)
+        self.assertEqual(len(main_for_test("45", "55", "55", "Y")), 0)
+        self.assertEqual(len(main_for_test("45", "55", "45", "Y")), 0)
+
+    def test_short_float_numbers(self):
+        # Test the shortest path creator function with float numbers
+        self.assertEqual(len(main_for_test("45.05", "55.03", "", "Y")), 0)
+        self.assertEqual(len(main_for_test("45.0566", "55.0566", "55", "Y")), 0)
+        self.assertEqual(len(main_for_test("45.000", "55.999", "45", "Y")), 0)
+
 #-----------------------------------------------------------------------------------------------------------------------
 
-    def test_any_path_good(self):
-        # Test the any path creator function with normal start and target word
+    def test_any_path_good_fourletters(self):
+        # Test the any path creator function with four-letter-long normal start and target word
         self.assertEqual(len(main_for_test("lead", "gold", "", "N")), 4)
         self.assertEqual(len(main_for_test("loss", "toys", "cold", "N")), 3)
         self.assertEqual(len(main_for_test("loss", "gold", "loss", "N")), 7)
+
+    def test_any_good_threeletters(self):
+        # Test the any path creator function with three-letter-long normal start and target word
+        self.assertEqual(len(main_for_test("led", "god", "", "jhf")), 3)
+        self.assertEqual(len(main_for_test("led", "god", "ged", "jhf")), 7)
+        self.assertEqual(len(main_for_test("led", "god", "gin", "jhf")), 3)
 
     def test_any_path_bad_start(self):
         # Test the any path creator function with bad start and normal target word
@@ -58,3 +76,15 @@ class TestWordLadder(unittest.TestCase):
         self.assertEqual(len(main_for_test("lead", "gin", "", " ")), 0)
         self.assertEqual(len(main_for_test("lead", "gin", "tin", " ")), 0)
         self.assertEqual(len(main_for_test("lead", "gin", "lean", " ")), 0)
+
+    def test_any_numbers(self):
+        # Test the shortest path creator function with numbers
+        self.assertEqual(len(main_for_test("45", "55", "", "d")), 0)
+        self.assertEqual(len(main_for_test("45", "55", "55", "f")), 0)
+        self.assertEqual(len(main_for_test("45", "55", "45", "g")), 0)
+
+    def test_any_float_numbers(self):
+        # Test the shortest path creator function with float numbers
+        self.assertEqual(len(main_for_test("45.05", "55.03", "", "n")), 0)
+        self.assertEqual(len(main_for_test("45.0566", "55.0566", "55", " ")), 0)
+        self.assertEqual(len(main_for_test("45.000", "55.999", "45", "")), 0)
