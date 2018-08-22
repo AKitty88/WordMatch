5435f43d96d272463c7041c9b4e55de7d4d1b5da
AKitty88 <a.kitty02@gmail.com>
2018-08-22 11:56:25 +1000

    test improved (without need of input)

--- word_ladder_test.py
+++ word_ladder_test.py
@@ -1,14 +1,12 @@
 import unittest
-from word_ladder import find_shortest
-from word_ladder import words
-
+from word_ladder_forTest import main_for_test
 
 class TestWordLadder(unittest.TestCase):
 
-    def test_word_normal_start_end(self):
-        # Test with normal start and target word
-        self.assertEqual(len(find_shortest("lead", "gold", words)), 4)
+    def test_short_with_normal_start_end(self):
+        # Test the shortest path creator function with normal start and target word
+        self.assertEqual(len(main_for_test("lead", "gold", "", "Y")), 4)
 
-    def test_word_bad_start_normal_end(self):
-        # Test with bad start and normal target word
-        self.assertEqual(len(find_shortest("lsss", "gold", words)), 0)
+    def test_short_with_bad_start_normal_end(self):
+        # Test the shortest path creator function with bad start and normal target word
+        self.assertEqual(len(main_for_test("lsss", "gold", "", "y")), 0)
